9fb804a733d4bade8725f28ee1c80dd871a8bc62
Neil Shepperd <nshepperd@gmail.com>
2013-11-27 18:05:10 +1100

    We now generate working Tupfiles. Success!

--- analyse.py
+++ analyse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 import sys, os
+from collections import namedtuple
 
 if len(sys.argv) < 4:
     print 'Usage: {} <log-file> <root> <output-file>'.format(sys.argv[0])
@@ -10,62 +11,82 @@ root = sys.argv[2]
 root = os.path.realpath(root)        
 to = sys.argv[3]
 
-pids = set()
-
-writes = {}
-reads = {}
-commands = {}
-cdirs = {}
+Operation = namedtuple('Operation', ['read', 'write', 'cwd', 'command'])
+operations = {}
 
 with open(logfile, 'r') as file:
     for line in file:
         line = eval(line)
         if line[1] in ('read', 'write'):
+            # Add an entry to their inputs/outputs.
+            # Note, the command itself should always be listed before any inputs and outputs.
             (pid, op, fname) = line
-            if pid not in writes:
-                writes[pid] = set()
-            if pid not in reads:
-                reads[pid] = set()
+            # If not, discard it
+            if pid not in operations:
+                # print 'Discarding irrelevant action', pid
+                continue
             if op == 'read':
-                reads[pid].add(os.path.relpath(fname, root))
+                # print 'read:', pid, os.path.relpath(fname, root)
+                operations[pid].read.add(os.path.relpath(fname, root))
             else:
                 assert op == 'write'
-                writes[pid].add(os.path.relpath(fname, root))
+                # print 'write:', pid, os.path.relpath(fname, root)
+                operations[pid].write.add(os.path.relpath(fname, root))
         else:
+            # Create a new command
             (pid, cwd, command) = line
-            # strip bullshit from the paths
-            sub_root = os.path.join(os.path.join(root, '.mnt'), root.lstrip('/'))
-            rel_root = os.path.relpath(sub_root, cwd)
-            cwd = cwd.replace(sub_root, '').lstrip('/')
+            # print 'create:', pid
+            # Paths in the recorded command line are always of the form /root/.mnt/root/...
+            # We need to clean this up so the build can be relocated.
+            sub_root = os.path.join(os.path.join(root, '.mnt'), root.lstrip('/')) # /root/.mnt/root
+            rel_root = os.path.relpath(sub_root, cwd)                             # cwd -> root 
+            cwd = cwd.replace(sub_root, '').lstrip('/') or '.'                    # root -> cwd
+            # Strip out all absolute paths (/root/.mnt/root) from commands
             command = [x.replace(sub_root, rel_root) for x in command]
-            pids.add(pid)
-            cdirs[pid] = cwd
-            commands[pid] = command
+            operations[pid] = Operation(set(), set(), cwd, command)
 
-entries = []
+def write_makefile(fname):
+    entries = []
+    all_outputs = []
+    for op in operations.values():
+        if 'Tpo' in ' '.join(op.command):
+            continue
+        if op.write:
+            read = sorted(op.read)
+            write = sorted(op.write)
+            all_outputs.extend(write)
+            entries.append('{write}: {read}\n\t(cd {cwd} && {command})'.format(
+                read=' '.join(read),
+                write=' '.join(write),
+                cwd=op.cwd,
+                command=' '.join(op.command)))
 
-for pid in pids:
-    if len(writes[pid]):
-        rd = sorted(reads[pid])
-        wr = sorted(writes[pid])
-        writedir = os.path.dirname(wr[0]) or '.'
-        if cdirs[pid] == '':
-            cdirs[pid] = '.'
-        entries.append('{}: {}\n\t(cd {} && {})'.format(' '.join(wr), ' '.join(rd), cdirs[pid], ' '.join(commands[pid])))
+    with open(fname, 'w') as file:
+        file.write('all: {}\n\n'.format(' '.join(all_outputs)))
+        file.write('clean:\n\trm -f {}\n\n'.format(' '.join(all_outputs)))
+        for item in sorted(entries):
+            file.write(item + '\n\n')
 
-        # entries.append(': {rd} |> (cd {cwd} && {cmd}) |> {wr}'.format(rd=' '.join(rd),
-        #                                                               wr=' '.join(wr),
-        #                                                               cwd=cdirs[pid],
-        #                                                               cmd=' '.join(commands[pid])))
+def write_tupfile(fname):
+    entries = []
+    all_outputs = []
+    for (_, op) in sorted(operations.items()):
+        # Sort by pid, so we get the topological sort required for tup.
+        if 'Tpo' in ' '.join(op.command):
+            continue
+        if op.write:
+            read = sorted(op.read)
+            write = sorted(op.write)
+            all_outputs.extend(write)
+            entries.append(': {read} |> (cd {cwd} && {command}) |> {write}'.format(
+                read=' '.join(read),
+                write=' '.join(write),
+                cwd=op.cwd,
+                command=' '.join(op.command)))
 
-def concat(gen):
-    res = []
-    for item in gen:
-        res.extend(item)
-    return res
+    with open(fname, 'w') as file:
+        for item in entries:
+            file.write(item + '\n\n')
 
-with open(to, 'w') as file:
-    file.write('all: {}\n\n'.format(' '.join(concat(writes.values()))))
-    file.write('clean:\n\trm -f {}\n\n'.format(' '.join(concat(writes.values()))))
-    for item in sorted(entries):
-        file.write(item + '\n\n')
+# write_makefile(to)
+write_tupfile(to)
