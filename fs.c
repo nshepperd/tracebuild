e2b8c734b3e89721715b6d0af99d446a724ed2f0
Neil Shepperd <nshepperd@gmail.com>
2013-12-04 14:37:46 +1100

    Don't mess up symlinks

--- fs.c
+++ fs.c
@@ -197,9 +197,7 @@ static int xmp_symlink(const char *from, const char *to)
   int res;
 
   char tmp1[4097];
-  char tmp2[4097];
   from = path_transform(tmp1, from);
-  to = path_transform(tmp2, to);
 
   res = symlink(from, to);
   if (res == -1)
